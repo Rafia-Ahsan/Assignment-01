fbeca80c339ba0cb9afdda64870d4a9f111c944b
Rafia Ahsan <160255825+Rafia-Ahsan@users.noreply.github.com>
2024-06-09 23:28:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//  QUESTION :03
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// declaring variable
+var firstName = "rafeya";
+// To convert in "Upper Case" --In capital letters
+console.log("UPPER CASE: ", firstName.toUpperCase());
+//  To convert in "Lower Case" --In Small Letters
+console.log("LOWER CASE: ", firstName.toLowerCase());
+//To convert in "Title Case" --Shows the name with the First Letter Big
+console.log("TITLE CASE: ", firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase());
